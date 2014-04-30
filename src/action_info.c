2abc50189b4e0dcd9f6b664194ce94e6a590b50e
uppfinnarn <uppfinnarn@gmail.com>
2014-05-01 01:32:09 +0200

    derp

--- src/action_info.c
+++ src/action_info.c
@@ -17,29 +17,17 @@ int action_info(int fcount, char **files)
 			SWFParser *parser = swf_parser_init();
 			SWF *swf = swf_parser_get_swf(parser);
 			
-			// -- This code triggers a flood of Error -127 (Invalid)
-			/*char buffer[kFileReadBufferSize];
+			char buffer[kFileReadBufferSize];
 			size_t size_read = 0;
-			while((size_read = fread(buffer, 1, sizeof(buffer), fp)) == sizeof(buffer))
+			while((size_read = fread(buffer, 1, sizeof(buffer), fp)) == 0)
 			{
 				SWFError err = swf_parser_append(parser, buffer, size_read);
 				if(err < 0)
 				{
 					fprintf(stderr, "libswf error (%d): %s\n", err, swf->err.text);
-					//break;
+					break;
 				}
 			}
-			*/
-			
-			fseek(fp, 0, SEEK_END);
-			size_t size = ftell(fp);
-			fseek(fp, 0, SEEK_SET);
-			
-			void *buffer = malloc(size);
-			fread(buffer, 1, size, fp);
-			SWFError err = swf_parser_append(parser, buffer, size);
-			if(err < 0)
-				fprintf(stderr, "libswf error (%d): %s\n", err, swf->err.text);
 			
 			swf_parser_free(parser);
 		}
