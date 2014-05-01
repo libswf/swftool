bef674163cd1bb292f59d5cf4522220acf01be83
uppfinnarn <uppfinnarn@gmail.com>
2014-05-01 11:59:22 +0200

    Chunked SWF reading that works (kinda)

--- src/action_info.c
+++ src/action_info.c
@@ -17,19 +17,31 @@ int action_info(int fcount, char **files)
 			SWFParser *parser = swf_parser_init();
 			SWF *swf = swf_parser_get_swf(parser);
 			
+			// This code can correctly duplicate a 34kb text file (`diff` says no difference)
+			// libswf errors out with SWF_INVALID instead, if fed the file in chunks
 			char buffer[kFileReadBufferSize];
 			size_t size_read = 0;
-			while((size_read = fread(buffer, 1, sizeof(buffer), fp)) == 0)
+			while((size_read = fread(buffer, 1, sizeof(buffer), fp)) > 0)
 			{
+				printf("Read %d bytes\n", size_read);
 				SWFError err = swf_parser_append(parser, buffer, size_read);
 				if(err < 0)
 				{
-					fprintf(stderr, "libswf error (%d): %s\n", err, swf->err.text);
+					fprintf(stderr, "libswf error (%d): %s\n", err, swf_parser_get_swf(parser)->err.text);
 					break;
 				}
 			}
 			
+			fclose(fp);
 			swf_parser_free(parser);
+			
+			printf("%s\n", path);
+			printf("SWF %d, %s compression\n", swf->version,
+				(swf->compression == SWF_UNCOMPRESSED ? "no" :
+					(swf->compression == SWF_ZLIB ? "ZLIB" :
+					(swf->compression == SWF_LZMA ? "LZMA" : "unknown"))));
+			
+			swf_free(swf);
 		}
 		else
 		{
