8fb4fabb9d54e252190c3c589ef0e46deb3b14bd
uppfinnarn <uppfinnarn@gmail.com>
2014-06-09 00:21:28 +0200

    Use a map to look up impl fptrs

--- src/main.c
+++ src/main.c
@@ -6,8 +6,20 @@
 // Typedef for function pointers to action implementations
 typedef int(*action_func_ptr)(int fcount, char **files);
 
+// Typedef for implementation map entries
+typedef struct {
+	const char *key;
+	action_func_ptr func;
+} action_entry;
+
 int main(int argc, char **argv)
 {
+	// Map of registered actions
+	action_entry actions[] = {
+		{ "info", &action_info },
+		{ "list", &action_list }
+	};
+	
 	// Pointer to the implementation of the chosen action; func ptrs are nice
 	// Default to printing a help message if there's no match
 	action_func_ptr action_func = &action_help;
@@ -16,10 +28,9 @@ int main(int argc, char **argv)
 	{
 		const char *action_str = argv[1];
 		
-		if(strcmp(action_str, "info") == 0)
-			action_func = &action_info;
-		else if(strcmp(action_str, "list") == 0)
-			action_func = &action_list;
+		for(unsigned i = 0; i < sizeof(actions)/sizeof(actions[0]); i++)
+			if(strcmp(action_str, actions[i].key) == 0)
+				{ action_func = actions[i].func; break; }
 	}
 	
 	return action_func((argc >= 3 ? argc - 2 : 0), argv + 2);
